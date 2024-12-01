02f7871c01e24b7f4713cb250bfa208b09e77dea
clementpeleman <clementpeleman@outlook.com>
2024-12-01 14:01:54 +0100

    added hero image

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -20,9 +20,16 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       className="relative -mt-[10.4rem] flex items-center justify-center text-white"
       data-theme="dark"
     >
-      <div className="container mb-8 z-10 relative flex items-center justify-center">
+      <div className=" backdrop-blur p-12 mb-8 z-10 relative flex items-center justify-center">
         <div className="max-w-[36.5rem] text-center">
-          {richText && <RichText className="mb-6" content={richText} enableGutter={false} />}
+          {richText && (
+            <RichText
+              className="mb-6"
+              content={richText}
+              enableGutter={false}
+              enableProse={false}
+            />
+          )}
           {Array.isArray(links) && links.length > 0 && (
             <ul className="flex justify-center gap-4">
               {links.map(({ link }, i) => {
