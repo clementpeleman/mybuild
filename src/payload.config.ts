408d6ce165e75036f668ca8f7e49850a98aa6cd8
Clement Peleman <clementpeleman@outlook.com>
2026-06-28 15:34:12 +0200

    Enable push feature in Vercel Postgres adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -55,6 +55,7 @@ export default buildConfig({
   // This config helps us configure global or default features that the other editors can inherit
   editor: defaultLexical,
   db: vercelPostgresAdapter({
+    push: true,
     pool: {
       connectionString: process.env.POSTGRES_URL || '',
     },
